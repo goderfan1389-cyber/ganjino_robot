2b2c65393de282c77677252446a0491e4b94f33f
goderfan1389-cyber <goderfan1389@gmail.com>
2026-09-17 06:25:15 +0330

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,9 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "1178057070:HPKhPZfmr8oVwDqSXIpbDFfqBroo
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "GANJINO_BOT")
 MAIN_GROUP_USERNAME = os.environ.get("MAIN_GROUP_USERNAME", "GANJINO_GAP")
 JOIN_CHANNEL_USERNAME = os.environ.get("JOIN_CHANNEL_USERNAME", "computer_program")
+BACKUP_PASSWORD = os.environ.get("BACKUP_PASSWORD", "GANJINO_TEAM_IR")
 
-ADMIN_IDS = [324157864, 890352247]  # آیدی عددی ادمین‌ها
+ADMIN_IDS = [324157864, 890352247]
 OWNER_ID = 324157864
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "dbname=postgres user=postgres password=postgres host=localhost port=5432")
